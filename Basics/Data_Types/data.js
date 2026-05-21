ea886bae6b7ee0bf08e59a2e2ed7292169a5bb63
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-05-21 13:48:18 +0600

    added data.js

---
+++ Basics/Data_Types/data.js
@@ -0,0 +1,20 @@
+//number date type
+let age = 19;
+
+//string data type
+let myName = "Alex";
+
+//boolean data type
+let isStudent = true;
+
+//undefined data type
+let schoolFee;
+
+//null data type
+let specialDiscount = null;
+
+console.log(typeof age);
+console.log(typeof myName);
+console.log(typeof schoolFee);
+console.log(typeof isStudent);
+console.log(typeof specialDiscount);
