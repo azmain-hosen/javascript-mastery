0f53b5aad09148a49e8170342b3dbbe35aaf5ca0
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-05-21 18:55:52 +0600

    added variable_practice.js

---
+++ Basics/Data_Types/non_primitive_data.js
@@ -0,0 +1,15 @@
+//special object. array data type
+let friends = ["adam", "deasa", "fadric", "alex"];
+
+//Object data type
+let myProfile = {
+  myName: "azmain",
+  age: 19,
+  isStudent: true,
+  schoolFee: null,
+  isRich: true,
+  friends: ["adam", "denasa", "fadric", "alex"],
+};
+
+console.log(typeof friends);
+console.log(typeof myProfile);
---
+++ Basics/Variable/variable_practice.js
@@ -0,0 +1,26 @@
+//var variable declaration
+var Name1 = "alex";
+var age1 = 19;
+var isRich1 = true;
+
+console.log(Name1);
+console.log(age1);
+console.log(isRich1);
+
+//const variable declaration
+const Name2 = "sofia";
+const age2 = 5;
+const isRich2 = false;
+
+console.log(Name2);
+console.log(age2);
+console.log(isRich2);
+
+//let variable declaration
+let Name3 = "farid";
+let age3 = 40;
+let isRich3 = true;
+
+console.log(Name3);
+console.log(age3);
+console.log(isRich3);
