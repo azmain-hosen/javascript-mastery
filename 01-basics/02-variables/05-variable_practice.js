3f34a924c6a16b577029f873e21b8c5d576f0608
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-08-23 21:42:36 +0600

    Add variable practice example

---
+++ 01-basics/02-variables/05-variable_practice.js
@@ -0,0 +1,15 @@
+let name = "Alex";
+name = "Al Frank";
+
+let age = 20;
+age = 25;
+
+const country = "Bangladesh";
+
+var isStudent = true;
+isStudent = false;
+
+console.log(name);
+console.log(age);
+console.log(country);
+console.log(isStudent);
