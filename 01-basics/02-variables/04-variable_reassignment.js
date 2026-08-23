8b13c438a67029d906faf0efb683e3520cc4bab4
azmain-hosen <md.azmainhosen.dev@gmail.com>
2026-08-23 21:32:09 +0600

    Add variable reassignment example

---
+++ 01-basics/02-variables/04-variable_reassignment.js
@@ -0,0 +1,13 @@
+var age1 = 19;
+console.log(age1);
+age1 = 20;
+console.log(age1);
+
+let age2 = 20;
+console.log(age2);
+age2 = 21;
+console.log(age2);
+
+const country = "Bangladesh";
+// country = "India";
+console.log(country);
